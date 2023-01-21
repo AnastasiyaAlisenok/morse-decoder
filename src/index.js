d210ac061e0d275a6170a8476a7019286d62e1e9
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-01-22 02:44:34 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    expr = [...expr]
+    let arrNew = [];
+    while(expr.length>0) {
+       let arr = expr.splice(0, 10);
+       arrNew.push(arr);
+    } 
+
+    arrNew.forEach(item => item.filter(e => {
+        if(e === '1') {
+           let index =  item.indexOf(e);
+        
+           item.splice(0, index);
+        } 
+    }
+    ))
+    
+    for(let i=0; i<arrNew.length; i++) {
+        arrNew[i] = arrNew[i].join('');
+        arrNew[i] = arrNew[i].replace(/10/g, '.');
+        arrNew[i] = arrNew[i].replace(/11/g, '-');
+    }
+
+    let text = [];
+    
+    for(let j=0; j<arrNew.length; j++) {
+        if(arrNew[j] === '**********') {
+            text[j] = ' ';
+        } else { 
+            text[j] = MORSE_TABLE[arrNew[j]];
+        }
+    }
+return text.join('');
 }
 
 module.exports = {
